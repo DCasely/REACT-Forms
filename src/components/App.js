e190141d66027e3c27bb1fb1ae0094970931152c
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-13 18:33:56 -0400

    added styling to form

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Form from './Form';
+import Header from './Header';
 
 export default function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [submittedName, setSubmittedName] = useState('');
-
-  function handleChangeFirstName(e) {
-    setFirstName(e.target.value);
-  }
-  function handleChangeLastName(e) {
-    setLastName(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    setSubmittedName(`${firstName} ${lastName}`);
-
-    console.log(submittedName);
-
-    e.preventDefault();
-  }
-
   return (
-    <div className="container">
-      <h1>Hello {submittedName}</h1>
-      <form>
-        <input
-          onChange={handleChangeFirstName}
-          value={firstName}
-          type="text"
-          placeholder="What's Your First Name?"
-        />
-        <input
-          onChange={handleChangeLastName}
-          value={lastName}
-          type="text"
-          placeholder="What's Your Last Name?"
-        />
-        <button onClick={handleSubmit} type="button">
-          Submit
-        </button>
-      </form>
+    <div>
+      <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [submittedName, setSubmittedName] = useState('');
+
+  function handleChangeFirstName(e) {
+    setFirstName(e.target.value);
+  }
+  function handleChangeLastName(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    setSubmittedName(`${firstName} ${lastName}`);
+
+    console.log(submittedName);
+
+    e.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {submittedName}</h1>
+      <form>
+        <input
+          onChange={handleChangeFirstName}
+          value={firstName}
+          type="text"
+          placeholder="What's Your First Name?"
+        />
+        <input
+          onChange={handleChangeLastName}
+          value={lastName}
+          type="text"
+          placeholder="What's Your Last Name?"
+        />
+        <button onClick={handleSubmit} type="button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
