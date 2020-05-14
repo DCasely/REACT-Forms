dd1a995d1846b868b29774d030da607ce568e344
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-14 12:02:39 -0400

    added email field

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,77 @@
 import React, { useState } from 'react';
 
 export default function Form() {
-  const [fullName, setFullName] = useState({
+  const [clientInfo, setClientInfo] = useState({
     firstName: '',
     lastName: '',
+    email: '',
   });
 
-  const [showData, setShowData] = useState('');
+  const [showFullName, setShowFullName] = useState('');
+  const [showEmail, setShowEmail] = useState('');
 
   function getData(e) {
     const { value, name } = e.target;
 
-    setFullName((prevValue) => {
+    setClientInfo((prevValue) => {
       if (name === 'firstName') {
-        return { firstName: value, lastName: prevValue.lastName };
+        return {
+          firstName: value,
+          lastName: prevValue.lastName,
+          email: prevValue.email,
+        };
       } else if (name === 'lastName') {
-        return { firstName: prevValue.firstName, lastName: value };
+        return {
+          firstName: prevValue.firstName,
+          lastName: value,
+          email: prevValue.email,
+        };
+      } else if (name === 'email') {
+        return {
+          firstName: prevValue.firstName,
+          lastName: prevValue.lastName,
+          email: value,
+        };
       }
     });
   }
 
   function displayData(e) {
-    setShowData(`${fullName.firstName} ${fullName.lastName}`);
+    setShowFullName(`${clientInfo.firstName} ${clientInfo.lastName}`);
+    setShowEmail(clientInfo.email);
     e.preventDefault();
   }
 
   return (
     <div className="container">
-      <h1>Hello {showData}</h1>
+      <h1>Hello {showFullName}</h1>
+
       <form onSubmit={displayData}>
         <input
           onChange={getData}
           name="firstName"
-          value={fullName.firstName}
+          value={clientInfo.firstName}
           type="text"
           placeholder="What's Your First Name?"
         />
         <input
           onChange={getData}
           name="lastName"
-          // value={fullName.lastName}
+          value={clientInfo.lastName}
           type="text"
           placeholder="What's Your Last Name?"
         />
+        <input
+          onChange={getData}
+          name="email"
+          value={clientInfo.email}
+          type="text"
+          placeholder="What's Your Email?"
+        />
         <button>Submit</button>
       </form>
+
+      <h2>Email: {showEmail} </h2>
     </div>
   );
 }
